16cdeec2af1fd9739e857950dd4ba8ab7547425e
Tech-Mex6 <okekemeekam@gmail.com>
2024-07-15 12:06:32 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by meekam okeke on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
