da843d6e2a801da68a2cc2c28a905624da855fca
Tech-Mex6 <okekemeekam@gmail.com>
2024-07-16 11:26:35 -0500

    Detailed comments added to explain each method's functionality.

--- Fetch/ViewModel/MealViewModel.swift
+++ Fetch/ViewModel/MealViewModel.swift
@@ -27,6 +27,15 @@ class MealViewModel: ObservableObject {
         }
     }
     
+    //MARK: - Fetch Meals
+    /// - Parameters: None
+    /// - Description:
+    /// Checks if cached meals exist. if they do, it loads them and updates `meals` on the main thread.
+    /// if no cached meals are found, it fetches meals by making an api call and then it updates `meals` on the
+    /// main thread and saves the fetched meals to the cache.
+    /// - Error Handling:
+    /// Prints an error message if fetching from the network fails.
+
     func fetchMeals() async {
         if LocalFileManager.instance.fileExists(fileName: fileName) {
             if let cachedMeals = LocalFileManager.instance.getData(from: fileName, as: [Meal].self) {
@@ -49,7 +58,12 @@ class MealViewModel: ObservableObject {
         }
     }
     
-    
+    //MARK: - Load Meal content for additional details
+    /// - Parameters:
+    ///  - id: The ID of the meal to fetch details for.
+    /// - Description: Fetches meal content for a specific meal ID from the network and updates `mealContent` on the main thread
+    /// - Error Handling: Prints an error message if fetching the meal content fails.
+
     func loadMealContent(id: String) async {
         do {
            let mealContent = try await NetworkingManager.shared.fetchMealContents(id: id)
