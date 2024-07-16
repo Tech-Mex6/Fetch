7aab3d94b2bf3d1fd1cb28b63ddc2927c5eb1cf7
Tech-Mex6 <okekemeekam@gmail.com>
2024-07-16 11:16:24 -0500

    Detailed comments added to explain the functionality of the method

--- Fetch/Extensions/UIApplication.swift
+++ Fetch/Extensions/UIApplication.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 import SwiftUI
-
+/// This extension adds a utility function to the `UIApplication` class to simplify ending text field editing across the entire application.
 extension UIApplication {
-    
+    //MARK: - End Editing
+    /// - Type: `Void`
+    /// - Parameters: None
+    /// - Description: This method sends an action to resign the first responder status from any responder object that is currently the first responder in the application.
+    /// Essentially, this is used to dismiss the keyboard or end text editing for the current text input view.
+    /// - Returns: None
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
